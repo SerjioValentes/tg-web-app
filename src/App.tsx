b1f99e9000e60a8f0b115e98b912572ab7fbef21
SerjioValentes <vasilkamalov@gmail.com>
2024-02-19 21:13:06 +0300

    web app 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import './main.css'
 function App() {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const tg = (window as any).Telegram?.webApp
-
+const onClose = () => {
+  if (tg) {
+    tg.close()
+  }
+}
   return (
-    <>
+    <div className="app-main">
       <div>
        Version 0.1
       </div>
       <h1>User - {tg && tg.initDataUnsafe.user?.username}</h1>
       <div className="card">
-        <button>count is</button>
+        <button onClick={onClose}>count is</button>
       </div>
-    </>
+    </div>
   )
 }
 
