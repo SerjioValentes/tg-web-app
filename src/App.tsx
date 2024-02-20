1a6c3dffb6271436916dd01bcec89d4e5e27bf3b
SerjioValentes <vasilkamalov@gmail.com>
2024-02-20 10:32:49 +0300

    added some css styles and header to bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import Header from './components/header'
 import './main.css'
 function App() {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const tg = (window as any).Telegram?.WebApp
-const onClose = () => {
-  if (tg) {
-    tg.close()
+  const tg = (window as any).Telegram?.WebApp
+  const onClose = () => {
+    if (tg) {
+      tg.close()
+    }
   }
-}
   return (
-    <div className="app-main">
+    <div className="app-main-wrapper">
+
       <div>
-       Version 0.4
+        <button onClick={onClose}>Close</button>
+        <div>Version 0.4</div>
+        <h1>User - {tg && tg.initDataUnsafe?.user}</h1>
+        <h1>version - {tg && tg.version}</h1>
       </div>
-      <h1>User - {tg && tg.initDataUnsafe?.user?.first_name}</h1>
-      <h1>User - {tg && tg.initDataUnsafe?.user}</h1>
-      <h1>version - {tg && tg.version}</h1>
-      <div className="card">
-        <button onClick={onClose}>count is</button>
+      
+      <div className='app-main-header'>
+        <Header />
       </div>
     </div>
   )
---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react"
+import './index.css'
+
+const Header = () => {
+    // const [state, setState] = useState()
+    return(
+        <div className="header">
+            <div>Menu 1</div>
+            <div>Menu 2</div>
+            <div>Menu 3</div>
+        </div>
+    )
+}
+
+export default Header
